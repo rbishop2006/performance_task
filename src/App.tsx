e5e315ce200e8c6d5149f1057c872f66d3c19d9c
Robert Bishop <rbishop2006@gmail.com>
2021-07-29 15:58:46 -0700

    installed antdesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import Main from './ui/layout/Main';
-import Navbar from './ui/layout/Navbar';
+import React from "react";
+import Main from "./ui/layout/Main";
+import Navbar from "./ui/layout/Navbar";
+import "antd/dist/antd.css";
 
 const App: React.FC = () => {
     return (
